b61c83691a729c408784eb6b59e9c0045379d111
NotSoSuper <NotSoSuper@users.noreply.github.com>
2017-06-30 16:35:00 -0400

    update setup requirements

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
   author_email = 'anorov.vorona@gmail.com',
   url = 'https://github.com/Anorov/cloudflare-scrape',
   keywords = ['cloudflare', 'scraping'],
-  install_requires = ['PyExecJS >= 1.4.0', 'requests >= 2.0.0']
+  install_requires = ['PyExecJS >= 1.4.0', 'aiohttp>=2.0.0']
 )
